190615f60e8c9d1fc39bf71d8395dabf88a57365
Pratyush Verma <pratyush.verma@flipkart.com>
2015-04-26 01:03:14 +0530

    added follows table

--- dao/src/main/resources/create.sql
+++ dao/src/main/resources/create.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS `login`;
 DROP TABLE IF EXISTS `like_dislikes`;
 DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `follows`;
 
 CREATE TABLE `login` (
   `account_number` int(50) NOT NULL AUTO_INCREMENT,
@@ -11,15 +12,19 @@ CREATE TABLE `login` (
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `like_dislikes` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `account_id` varchar(150) NOT NULL DEFAULT '',
   `product_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`account_id`,`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `account_id` varchar(50) NOT NULL DEFAULT '',
   `name` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `follows` (
+  `followed_by` varchar(50) NOT NULL DEFAULT '',
+  `follows` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`followed_by`,`follows`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;

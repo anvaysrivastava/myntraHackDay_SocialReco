58f76699dc8298b58bb88668a9e7546f6302779b
Pratyush Verma <pratyush.verma@flipkart.com>
2015-04-25 19:41:28 +0530

    updated mysql

--- dao/src/main/resources/create.sql
+++ dao/src/main/resources/create.sql
@@ -1,9 +1,3 @@
-CREATE TABLE `like_dislikes` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `account_id` varchar(150) NOT NULL DEFAULT '',
-  `product_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
 CREATE TABLE `login` (
   `account_number` int(50) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
@@ -11,3 +5,10 @@ CREATE TABLE `login` (
   PRIMARY KEY (`email`),
   UNIQUE KEY `account_number` (`account_number`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `like_dislikes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(150) NOT NULL DEFAULT '',
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;

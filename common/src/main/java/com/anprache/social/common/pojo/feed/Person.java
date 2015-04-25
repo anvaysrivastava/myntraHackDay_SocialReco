f6fca93916d448263453ffc6c826067c22832a3d
Anvay Srivastava <anvay.srivastava@flipkart.com>
2015-04-25 16:56:34 +0530

    Feed api mock is up

---
+++ common/src/main/java/com/anprache/social/common/pojo/feed/Person.java
@@ -0,0 +1,20 @@
+package com.anprache.social.common.pojo.feed;
+
+/**
+ * Created by anvay.srivastava on 25/04/15.
+ */
+public class Person {
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
